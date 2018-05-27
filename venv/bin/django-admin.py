3c103fac7b47bd1862a672a172c0735d99f5282b
Charles Morey <charlesmorey@MacBook-Pro-de-Charles.local>
2018-05-27 20:16:56 +0200

    All files added on git YEY

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/charlesmorey/Documents/Hetic/2017 - 2018 /Python /BlogPython/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
